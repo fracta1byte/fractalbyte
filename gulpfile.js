10706364e9a223799bab12beaad11bec8c673c68
tr0ythex <tr0ythex@gmail.com>
2016-08-29 12:05:52 +0300

    Удален lite-server. Добавлен browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,25 @@
 /* jshint esversion: 6 */
 
 const gulp = require('gulp'),
-      inject = require('gulp-inject');
+      inject = require('gulp-inject'),
+      browserSync = require('browser-sync');
 
 gulp.task('index', () => {
   const target = gulp.src('./app/index.html');
   const sources = gulp.src(['./bower_components/**/*.min.js', './bower_components/**/*.min.css'], {read: false});
 
   target.pipe(inject(sources)).pipe(gulp.dest('./app'));
+});
+
+gulp.task('serve', () => {
+  browserSync({
+    notify: false,
+    port: 8000,
+    server: {
+      baseDir: ['app'],
+      routes: {
+        '/bower_components': 'bower_components'
+      }
+    }
+  });
 });
