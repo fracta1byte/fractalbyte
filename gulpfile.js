ea9e20b13b39cca66b80ca7bc4fef500e9d4014b
tr0ythex <tr0ythex@gmail.com>
2016-08-29 17:57:23 +0300

    Добавлен и настроен bootstrap-sass. Организована стилевая структура. Добавлены gulp таски на sass-to-css компиляцию, вставку bower зависимостей (wiredep), сборку css файлов в один (useref)

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,26 @@
 
 const gulp = require('gulp'),
       inject = require('gulp-inject'),
-      browserSync = require('browser-sync');
+      browserSync = require('browser-sync'),
+      sass = require('gulp-sass'),
+      wiredep = require('wiredep').stream,
+      useref = require('gulp-useref');
 
-gulp.task('index', () => {
+// Инжектировать все min.js и min.css файлы из bower_components в index.html
+// между <!-- inject:{js,css} --> <!-- endinject -->
+gulp.task('inject', () => {
   const target = gulp.src('./app/index.html');
-  const sources = gulp.src(['./bower_components/**/*.min.js', './bower_components/**/*.min.css'], {read: false});
+  const sources = gulp.src([
+    './bower_components/**/*.min.js',
+    './bower_components/**/*.min.css'
+  ], {read: false});
 
   target.pipe(inject(sources)).pipe(gulp.dest('./app'));
 });
 
+// Запустить dev-сервер browserSync
+// baseDir - папка с файлом index.html (откуда стартует приложение)
+// routes - маршруты для дополнительных подгружаемых скриптов и стилей
 gulp.task('serve', () => {
   browserSync({
     notify: false,
@@ -22,4 +33,34 @@ gulp.task('serve', () => {
       }
     }
   });
+});
+
+// Скомпилировать .sass в .css
+gulp.task('sass', () => {
+  gulp.src('app/styles/sass/**/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/styles'));
+});
+
+// Вставить bower-зависимости, прописанные в bower.json dependencies
+// в файл vendor.sass между <!-- bowers:scss --> и <!-- endbower -->
+gulp.task('wiredep', () => {
+  gulp.src('app/styles/sass/vendor.sass')
+    .pipe(wiredep())
+    .pipe(gulp.dest('app/styles/sass'));
+});
+
+// useref находит в index.html файле участок
+// <!-- build:css styles/vendor.css --> <!-- endbuild -->
+// собирает все файлы между этими комментариями и делает из них один файл,
+// в данном случае vendor.css
+/*
+  СAUTION: запускать только когда появится папка dist, иначе стирает участок
+  <!-- build:css styles/vendor.css --> <!-- endbuild -->
+*/
+gulp.task('useref', () => {
+    gulp.src('app/index.html')
+        .pipe(useref())
+        // TODO: поменять на папку dist (или что-то в этом духе)
+        .pipe(gulp.dest('app'));
 });
