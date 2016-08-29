750ce1f970db9db125e1fc7cf09bc1c125c88ffa
Dmitriy Dolinin <dmitry5151@yandex.ru>
2016-08-29 22:24:28 +0300

    Отступы исправлены с 2 на 4 пробела

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Fractal Byte Company
+====================
--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,62 @@
 /* jshint esversion: 6 */
 
 const gulp = require('gulp'),
-      inject = require('gulp-inject'),
-      browserSync = require('browser-sync'),
-      sass = require('gulp-sass'),
-      wiredep = require('wiredep').stream,
-      useref = require('gulp-useref'),
-      reload = browserSync.reload;
+    inject = require('gulp-inject'),
+    browserSync = require('browser-sync'),
+    sass = require('gulp-sass'),
+    wiredep = require('wiredep').stream,
+    useref = require('gulp-useref'),
+    reload = browserSync.reload;
 
 // Инжектировать все min.js и min.css файлы из bower_components в index.html
 // между <!-- inject:{js,css} --> <!-- endinject -->
 gulp.task('inject', () => {
-  const target = gulp.src('./app/index.html');
-  const sources = gulp.src([
-    './bower_components/**/*.min.js',
-    './bower_components/**/*.min.css'
-  ], {read: false});
+    const target = gulp.src('./app/index.html');
+    const sources = gulp.src([
+        './bower_components/**/*.min.js',
+        './bower_components/**/*.min.css'
+    ], {
+        read: false
+    });
 
-  target.pipe(inject(sources)).pipe(gulp.dest('./app'));
+    target.pipe(inject(sources)).pipe(gulp.dest('./app'));
 });
 
 // Запустить dev-сервер browserSync
 // baseDir - папка с файлом index.html (откуда стартует приложение)
 // routes - маршруты для дополнительных подгружаемых скриптов и стилей
 gulp.task('serve', () => {
-  browserSync({
-    notify: false,
-    port: 8000,
-    server: {
-      baseDir: ['app'],
-      routes: {
-        '/bower_components': 'bower_components'
-      }
-    }
-  });
-  
-  gulp.watch('app/*.html').on('change', reload);
-  gulp.watch('app/styles/**/*.sass', ['sass']);
+    browserSync({
+        notify: false,
+        port: 8000,
+        server: {
+            baseDir: ['app'],
+            routes: {
+                '/bower_components': 'bower_components'
+            }
+        }
+    });
+
+    gulp.watch('app/*.html').on('change', reload);
+    gulp.watch('app/styles/**/*.sass', ['sass']);
 });
 
 // Скомпилировать .sass в .css
 gulp.task('sass', () => {
-  gulp.src('app/styles/sass/**/*.sass')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('app/styles'))
-    .pipe(reload({stream: true})); // перезагрузить browserSync
+    gulp.src('app/styles/sass/**/*.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('app/styles'))
+        .pipe(reload({
+            stream: true
+        })); // перезагрузить browserSync
 });
 
 // Вставить bower-зависимости, прописанные в bower.json dependencies
 // в файл vendor.sass между <!-- bowers:scss --> и <!-- endbower -->
 gulp.task('wiredep', () => {
-  gulp.src('app/styles/sass/vendor.sass')
-    .pipe(wiredep())
-    .pipe(gulp.dest('app/styles/sass'));
+    gulp.src('app/styles/sass/vendor.sass')
+        .pipe(wiredep())
+        .pipe(gulp.dest('app/styles/sass'));
 });
 
 // useref находит в index.html файле участок
