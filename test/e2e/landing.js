db85291bcfa0a133cfa99869d6125ecd2b0aa97c
Dmitriy Dolinin <dmitry5151@yandex.ru>
2016-09-01 20:18:35 +0300

    Все отступы переведены на 4 пробела

--- test/e2e/landing.js
+++ test/e2e/landing.js
@@ -1,11 +1,11 @@
 /* jshint esversion: 6 */
 
 const webdriver = require('selenium-webdriver'),
-      phantomjs = require('selenium-webdriver/phantomjs');
+    phantomjs = require('selenium-webdriver/phantomjs');
 
 const driver = new webdriver.Builder()
-  .forBrowser('phantomjs')
-  .build();
+    .forBrowser('phantomjs')
+    .build();
 
 const chai = require('chai'),
     expect = chai.expect;
@@ -13,22 +13,22 @@ const chai = require('chai'),
 
 describe('Landing title', () => {
 
-  before((done) => {
-    driver.navigate().to('http://localhost:8000')
-    .then(() => done());
-  });
+    before((done) => {
+        driver.navigate().to('http://localhost:8000')
+            .then(() => done());
+    });
 
-  it('should contain words Fractal Byte', (done) => {
-    driver.getTitle()
-    .then((title) => {
-      expect(title).to.be.equal('Fractal Byte');
-    })
-    .then(() => done());
-  });
+    it('should contain words Fractal Byte', (done) => {
+        driver.getTitle()
+            .then((title) => {
+                expect(title).to.be.equal('Fractal Byte');
+            })
+            .then(() => done());
+    });
 
-  after((done) => {
-    driver.quit()
-    .then(() => done());
-  });
+    after((done) => {
+        driver.quit()
+            .then(() => done());
+    });
 
 });
